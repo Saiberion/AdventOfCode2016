4180603d6d5dd32529cfec40a619a31def946832
Saiberion <saiberion@web.de>
2019-01-18 13:55:21 +0100

    AoC Helper hinzugefügt

---
+++ AoCHelpers/InputLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AoCHelpers
+{
+    public class InputLoader
+    {
+        public static List<string> LoadByLines(string filename)
+        {
+            FileStream file = new FileStream(filename, FileMode.Open);
+            StreamReader stream = new StreamReader(file);
+            string line;
+            List<string> lines = new List<string>();
+
+            while ((line = stream.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            stream.Dispose();
+            file.Dispose();
+            return lines;
+        }
+    }
+}
