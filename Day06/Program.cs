c4ef002c7a2cf3cf58a3556aed9d6d9afea618b7
Saiberion <saiberion@web.de>
2019-01-31 14:58:32 +0100

    Day06 solution complete

---
+++ Day06/Program.cs
@@ -0,0 +1,75 @@
+﻿using AocHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06
+{
+    class Program
+    {
+        static string RepetitionCorrection(List<string> input)
+        {
+            StringBuilder sbP1 = new StringBuilder();
+            StringBuilder sbP2 = new StringBuilder();
+
+            Dictionary<char, int>[] charCountingByPosition = new Dictionary<char, int>[input[0].Length];
+
+            for (int i = 0; i < input[0].Length; i++)
+            {
+                charCountingByPosition[i] = new Dictionary<char, int>();
+            }
+
+            foreach(string s in input)
+            {
+                for(int i = 0; i < s.Length; i++)
+                {
+                    if (charCountingByPosition[i].ContainsKey(s[i]))
+                    {
+                        charCountingByPosition[i][s[i]]++;
+                    }
+                    else
+                    {
+                        charCountingByPosition[i].Add(s[i], 1);
+                    }
+                }
+            }
+
+            for(int i = 0; i < charCountingByPosition.Length; i++)
+            {
+                int max = int.MinValue;
+                int min = int.MaxValue;
+                char c1 = char.MinValue;
+                char c2 = char.MinValue;
+                foreach (KeyValuePair<char, int> kvp in charCountingByPosition[i])
+                {
+                    if (kvp.Value > max)
+                    {
+                        c1 = kvp.Key;
+                        max = kvp.Value;
+                    }
+
+                    if (kvp.Value < min)
+                    {
+                        c2 = kvp.Key;
+                        min = kvp.Value;
+                    }
+                }
+                sbP1.Append(c1);
+                sbP2.Append(c2);
+            }
+
+            sbP1.Append(" - ");
+            sbP1.Append(sbP2);
+            return sbP1.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> input = InputLoader.LoadByLines("input.txt");
+            Console.WriteLine("corrected message: {0}", RepetitionCorrection(input));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day06/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Day06")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day06")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("22a4ba42-91ee-4752-ad9c-5b02e4d7d4c1")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
