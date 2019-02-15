51ec1ca69269dc66477550b19c648a675f14063a
Saiberion <saiberion@web.de>
2019-02-15 17:15:45 +0100

    Day 20 Part 1 solved

---
+++ Day20/Program.cs
@@ -0,0 +1,81 @@
+﻿using AocHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20
+{
+    class Program
+    {
+        static long MinimumFreeIP(List<string> input)
+        {
+            List<long[]> ranges = new List<long[]>();
+            foreach(string s in input)
+            {
+                string[] splitted = s.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+                ranges.Add(new long[] { long.Parse(splitted[0]), long.Parse(splitted[1]) });
+            }
+
+            long minAllowed = uint.MaxValue;
+
+            long minimumBlocked = uint.MaxValue;
+            long[] minRange = null;
+            foreach (long[] arr in ranges)
+            {
+                if (minimumBlocked > arr[0])
+                {
+                    minimumBlocked = arr[0];
+                    minRange = arr;
+                }
+            }
+
+            if (minRange[0] > 0)
+            {
+                return 0;
+            }
+            else
+            {
+                minAllowed = minRange[1] + 1;
+                ranges.Remove(minRange);
+            }
+
+            do
+            {
+                minimumBlocked = uint.MaxValue;
+                foreach (long[] arr in ranges)
+                {
+                    if (minimumBlocked > arr[0])
+                    {
+                        minimumBlocked = arr[0];
+                        minRange = arr;
+                    }
+                }
+                if ((minRange[0] < minAllowed) && (minRange[1] < minAllowed))
+                {
+                    // ignore, this range is already dead
+                    ranges.Remove(minRange);
+                }
+                else if ((minRange[0] > minAllowed) && (minRange[1] > minAllowed))
+                {
+                    // no range is starting earlier, result found
+                    return minAllowed;
+                }
+                else
+                {
+                    // overlapping region
+                    minAllowed = minRange[1] + 1;
+                    ranges.Remove(minRange);
+                }
+            } while (true);
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> input = InputLoader.LoadByLines("input.txt");
+            Console.WriteLine("Dragon checksum disc 1: {0}", MinimumFreeIP(input));
+            Console.ReadLine();
+        }
+    }
+}
